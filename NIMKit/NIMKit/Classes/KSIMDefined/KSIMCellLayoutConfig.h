0be1627305c3b37417918339de64556eec80c3d4
wliu6 <1029769711@qq.com>
2018-04-17 14:33:51 +0800

    添加自定义 cellconfig

---
+++ NIMKit/NIMKit/Classes/KSIMDefined/KSIMCellLayoutConfig.h
@@ -0,0 +1,12 @@
+//
+//  KSIMCellLayoutConfig.h
+//  KSIMUIKit
+//
+//  Created by 王铎睿 on 2018/4/17.
+//
+
+#import "NIMCellLayoutConfig.h"
+
+@interface KSIMCellLayoutConfig : NIMCellLayoutConfig <NIMCellLayoutConfig>
+
+@end
---
+++ NIMKit/NIMKit/Classes/KSIMDefined/KSIMCellLayoutConfig.m
@@ -0,0 +1,18 @@
+//
+//  KSIMCellLayoutConfig.m
+//  KSIMUIKit
+//
+//  Created by 王铎睿 on 2018/4/17.
+//
+
+#import "KSIMCellLayoutConfig.h"
+
+@implementation KSIMCellLayoutConfig
+- (CGSize)contentSize:(NIMMessageModel *)model cellWidth:(CGFloat)width
+{
+    if (model.me) {
+        <#statements#>
+    }
+    return [super contentSize:model cellWidth:width];
+}
+@end
