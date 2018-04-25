02f27b92f6692f49fd0e7e9846b1b2ac2c6e17a6
wliu6 <1029769711@qq.com>
2018-04-25 15:11:00 +0800

    利用动态语言特性-不去model化的router规划开始

---
+++ NIMKit/NIMKit/Classes/KSIMDefined/KSZCMediateRouter/KSZCVanillaRouter.h
@@ -0,0 +1,13 @@
+//
+//  KSZCVanillaRouter.h
+//  KSIMUIKit
+//
+//  Created by 王铎睿 on 2018/4/25.
+//
+
+#import <Foundation/Foundation.h>
+
+// 猥琐流 router
+@interface KSZCVanillaRouter : NSObject
+
+@end
---
+++ NIMKit/NIMKit/Classes/KSIMDefined/KSZCMediateRouter/KSZCVanillaRouter.m
@@ -0,0 +1,12 @@
+//
+//  KSZCVanillaRouter.m
+//  KSIMUIKit
+//
+//  Created by 王铎睿 on 2018/4/25.
+//
+
+#import "KSZCVanillaRouter.h"
+
+@implementation KSZCVanillaRouter
+
+@end
